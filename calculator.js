6d5a751363499781b88ab5e43ca39f3c3ed5c6f8
Cristian <cristiangerardchen@gmail.com>
2023-07-28 15:26:57 +0800

    Complete visual interface

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let firstNum, secondNum, op;
+
+function operate() {
+
+}
+
+
+/*
+
+A) do up interface
+
+1) when key in number - shows up on screen
+more numbers can be added to the screen
+
+2) after press operator - "capture" full number
+
+3) repeat 1)
+
+4) after press = (or other operators) - "capture" full number and 
+add it to the first number. display answer
+
+5) IF press other operators again, "capture" latest answer and repeat 3)
