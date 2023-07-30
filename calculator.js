eeb2bcb659a39093245d49d0b4e76d27bcc7a456
Cristian <cristiangerardchen@gmail.com>
2023-07-30 22:40:36 +0800

    create ability to operate on two inputs

--- calculator.js
+++ calculator.js
@@ -1,25 +1,84 @@
-/*let firstNum, secondNum, op;
-
-function operate() {
-
-}
-*/
+let firstNum, secondNum , answer;
+let op="";
 
 const display=document.querySelector(".display");
 
-const numButtons=document.querySelectorAll(".numButton")
+const numButtons=document.querySelectorAll(".numButton");
 
-let displayText;
-    
+let displayText="";
+let displayText2="";
+
+const opButtons=document.querySelectorAll(".opButton");
+
 numButtons.forEach(function(numButton){
     numButton.addEventListener("click", function() {
         displayText+=numButton.id;
         display.textContent=displayText;
+})
+})
+
+opButtons.forEach(function(opButton){
+    opButton.addEventListener("click", function() {
+        firstNum=displayText;
+        op=opButton.id;     
+
+        if (op === "+" || op === "-" || op === "/" || op === "*") {
+            numButtons.forEach(function(numButton){
+            numButton.addEventListener("click", function() {
+                displayText2+=numButton.id;
+                display.textContent=displayText2;
+              })
+        })}
     })
 })
 
+function operate(){
+    secondNum=displayText2;
+    if (op === "+") answer = parseInt(firstNum) + parseInt(secondNum);
+    if (op === "-") answer = firstNum - secondNum;
+    if (op === "*") answer = firstNum*secondNum;
+    if (op === "/") answer = firstNum/secondNum;
+    display.textContent=answer;
+}
+
+const equalButton=document.querySelector(".equalButton");
+
+equalButton.addEventListener("click", operate)
 
 
+
+/*{
+    secondNum=displayText;
+    if (op === "+") answer = parseInt(firstNum) + parseInt(secondNum);
+    if (op === "-") answer = firstNum - secondNum;
+    if (op === "*") answer = firstNum*secondNum;
+    if (op === "/") answer = firstNum/secondNum;
+    firstNum=answer;
+    display.textContent=firstNum;
+
+    numButtons.forEach(function(numButton){
+        numButton.addEventListener("click", function() {
+            displayText="";
+            displayText+=numButton.id;
+            display.textContent=displayText;
+    })
+    })
+})
+
+*/
+
+// clear button - come back to it 
+   
+const clearButton=document.querySelector(".clearButton");
+
+clearButton.addEventListener ("click", function() {
+    display.textContent="";
+    firstNum="";
+    secondNum="";
+    displayText="";
+    op="";
+    answer="";
+})
 
 /*
 NOTE:
